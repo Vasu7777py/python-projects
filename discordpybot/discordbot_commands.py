9e5ce3672d3a4460fbfaf323c24eb2ba38796134
Vasu Subbannavar <65950340+Vasu7777py@users.noreply.github.com>
2020-06-01 16:22:03 +0530

    removing few stuffs

--- discordpybot/discordbot_commands.py
+++
@@ -1,1 +0,0 @@
-Commands = ["help", "show", "list", "leaderboard"]
--- discordpybot/test.py
+++
@@ -1,4 +0,0 @@
-import discord
-
-client = discord.Client()
-client.run("NzE1MDQ3NTI1OTc0ODAyNDQy.XtJrjg.eixkiFlnwoMbFI_iR56L_ospt68")
