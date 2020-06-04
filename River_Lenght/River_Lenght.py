0e3d00fa7900c0dd3e10f351dff3e75d4e4a7e5c
Vasu Subbannavar <65950340+Vasu7777py@users.noreply.github.com>
2020-06-04 12:32:24 +0530

    adding some code

--- River_Lenght/River_Lenght.py
+++ River_Lenght/River_Lenght.py
@@ -1,14 +1,20 @@
+import numpy as np
+
 def main():
-    lenght = int(input("Enter the lenght of the river : "))
-    breath = int(input("Enter the breath of the river : "))
-    land = []
-    print("Enter the land data : ")
+    lenght = int(input("Enter the lenght of the land : "))
+    breath = int(input("Enter the breath of the land : "))
+    land = np.ndarray(shape = (lenght, breath), dtype = int)
     for y in range(lenght):
-        land.append([])
-        for _ in range(breath):
-            land[y].append(int(input()))
-        print("")
-    print(land)            
+        for x in range(breath):
+            land_flag = False
+            while not land_flag:
+                land[y][x] = int(input(f"Enter the info of land {y}, {x} : "))
+                if ((land[y][x] == 1) or (land[y][x] == 0)):
+                    land_flag = True
+                else:
+                    print("Entered land info is not accetible")
+
+    print(land)
 
 if __name__ == "__main__":
     main()
