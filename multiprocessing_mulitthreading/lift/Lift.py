f714f2786afe7ea31361874671f33579833ff756
Vasu Subbannavar <65950340+Vasu7777py@users.noreply.github.com>
2020-06-13 18:41:23 +0530

    added some functions skletion

--- multiprocessing_mulitthreading/lift/Lift.py
+++ multiprocessing_mulitthreading/lift/Lift.py
@@ -3,12 +3,16 @@ from threading import Thread
 from time import sleep
 
 class Lift:
+
+    Request = []
+    Lifts = []
+
     def __init__(self, Number:int, Present_floor = 0):
         self.Number = Number
         self.Running = False
         self.Interception = False
         self.Request_list = []
-        self.Intercept_requst = []
+        self.Intercept_request = []
         self.Present_floor = Present_floor
         self.Destination = None
         self.Direction = None
@@ -16,6 +20,14 @@ class Lift:
     def time_requried(self, floor, direction):
         pass
     
+    @classmethod
+    def get_request(floor, direction):
+        pass
+
+    @classmethod
+    def set_request(floor, direction, lift:Lift):
+        pass
+
     def pick_up(self):
         pass
 
