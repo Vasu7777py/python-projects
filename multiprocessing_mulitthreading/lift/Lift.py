6c50b6f750ea59b912499d591d2048575338eb42
Vasu Subbannavar <65950340+Vasu7777py@users.noreply.github.com>
2020-06-14 18:24:08 +0530

    did some changes

--- multiprocessing_mulitthreading/lift/Lift.py
+++ multiprocessing_mulitthreading/lift/Lift.py
@@ -8,6 +8,8 @@ class Lift:
 
     Requests = []
     Lifts = []
+    speed = 1
+    wait = 6
 
     def __init__(self, Number:int, Present_floor = 0):
         self.Number = Number
@@ -20,7 +22,27 @@ class Lift:
         self.Direction = None
 
     def time_requried(self, floor, direction):
-        pass
+        #return_val = None
+        Threadlock.acquire()
+        if (floor > self.Present_floor):
+            if self.Running:
+                if (self.Direction == direction):
+                    if (self.Direction == 1):
+                        floor_diff = (floor - self.Present_floor)
+                    else:
+                        floor_diff = ((floor - self.Present_floor) + (2 * (self.Present_floor - self.Destination) ) )
+                elif ((direction == 1) and (self.Direction == -1)):
+                    floor_diff = ((floor - self.Present_floor) + (2 * (self.Present_floor - self.Destination) ) )
+                elif ((direction == -1) and (self.Direction == 1)):
+                    if (floor > self.Destination):
+                        floor_diff = (floor - self.Present_floor)
+                    else:
+                        floor_diff = ((2 * (self.Destination - floor) + (floor - )))
+            else:
+                pass
+        time_req = ((floor_diff * self.speed) + (len(self.Request_list) * self.wait))
+        Threadlock.release()
+        return (self,time_req)
     
     @classmethod
     def get_request(cls):
@@ -31,9 +53,10 @@ class Lift:
         for request in Requests:
             floor, direction = request
             for lift in cls.Lifts:
-                Time_Requried = lift.time_requried(floor, direction)
-                if (Time_Requried != None):
-                    lift.set_request(floor, direction, lift)
+                if request not in lift.Request_list:
+                    Time_Requried = lift.time_requried(floor, direction)
+                    if (Time_Requried != None):
+                        lift.set_request(floor, direction, lift)
 
     @classmethod
     def set_request(cls, floor, direction, lift:Lift):
@@ -44,3 +67,9 @@ class Lift:
 
     def start(self):
         pass
+
+
+Lift.Lifts.append(Lift(1))
+Lift.Lifts.append(Lift(2))
+Lift.Lifts.append(Lift(3))
+Lift.Lifts.append(Lift(4))
